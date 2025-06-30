8dd7060a600977569bd98a273ed7d326770b6dd9
korka-dev <diallo30amadoukorka@gmail.com>
2025-06-30 22:46:06 +0000

    feat: update config feat

--- app/config.py
+++ app/config.py
@@ -1,5 +1,4 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from urllib.parse import urlparse
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="allow")
@@ -10,33 +9,7 @@ class Settings(BaseSettings):
     algorithm: str = "HS256"
     cors_origin:str="*"
 
-    @property
-    def postgres_username(self) -> str:
-        parsed = urlparse(self.postgres_url)
-        return parsed.username or ""
-
-    @property
-    def postgres_password(self) -> str:
-        parsed = urlparse(self.postgres_url)
-        return parsed.password or ""
-
-    @property
-    def postgres_host(self) -> str:
-        parsed = urlparse(self.postgres_url)
-        return parsed.hostname or ""
-
-    @property
-    def postgres_port(self) -> int:
-        parsed = urlparse(self.postgres_url)
-        return parsed.port or 5432
-
-    @property
-    def postgres_database(self) -> str:
-        parsed = urlparse(self.postgres_url)
-        path = parsed.path
-        if path.startswith('/'):
-            path = path[1:]
-        return path
+  
 
     @property
     def postgres_database_url(self) -> str:
